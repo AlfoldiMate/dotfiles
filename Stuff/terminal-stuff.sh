6fa7352efd82a1a527446d67ea410fb7a8f8c1f7
Alföldi Máté <alfoldi.mate@gmail.com>
2026-02-08 23:35:40 +0100

    init script

---
+++ Stuff/terminal-stuff.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+brew install yadm
+
+brew install wezterm
+brew install starship
+
+brew install thefuck
+brew install eza
+brew install zoxide
+brew install tlrc
+brew install fzf
+brew intsall fd
+
+brew install yazi
