b91224ed762fb1211a2cbc8d695441c3e453bf3a
Alföldi Máté <alfoldi.mate@gmail.com>
2026-02-12 20:38:51 +0100

    lazyvim config and border revert

--- .config/nvim/lua/plugins/base.lua
+++ .config/nvim/lua/plugins/base.lua
@@ -1,9 +1,35 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
+    },
+  },
+  {
+    "tiagovla/scope.nvim",
+    config = true,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        color_icons = false,
+        show_buffer_close_icons = false,
+        always_show_bufferline = true,
+        auto_toggle_burreline = false,
+        separator_style = "thin",
+      },
+      highlights = {
+        buffer_selected = {
+          bold = false,
+          italic = false,
+        },
+      },
     },
   },
 }
