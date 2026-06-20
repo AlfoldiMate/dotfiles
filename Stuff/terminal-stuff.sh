6d8d1e294ee90ca9f2099c0bcac1f4960907106b
Alföldi Máté <alfoldi.mate@gmail.com>
2026-06-20 23:03:27 +0200

    Bootstrap: add bat/delta/atuin/fd/rg/extras + bat cache build

--- Stuff/terminal-stuff.sh
+++ Stuff/terminal-stuff.sh
@@ -11,5 +11,20 @@ brew install eza
 brew install zoxide
 brew install tlrc
 brew install fzf
+brew install fd
+brew install ripgrep
+
+brew install bat
+brew install git-delta
+brew install atuin
+brew install dust
+brew install sd
+brew install btop
+brew install glow
+brew install hyperfine
 
 brew install yazi
+brew install lazygit
+
+# Build bat theme cache (after the Catppuccin theme is in place via yadm)
+bat cache --build
